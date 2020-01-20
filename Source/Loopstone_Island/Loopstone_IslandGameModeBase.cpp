fc10ccde807e241d2c9f4279649d2a38287adec5
iKnewt <iknewt@gmail.com>
2020-01-20 13:02:36 +0100

    added original lighting and player controls

--- Source/Loopstone_Island/Loopstone_IslandGameModeBase.cpp
+++ Source/Loopstone_Island/Loopstone_IslandGameModeBase.cpp
@@ -2,4 +2,16 @@
 
 
 #include "Loopstone_IslandGameModeBase.h"
+#include "PlayerCharacter.h"
+#include "ConstructorHelpers.h"
 
+ALoopstone_IslandGameModeBase::ALoopstone_IslandGameModeBase()
+: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Assets/Characters/Player/BP_PlayerCharacter.uasset"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	// HUDClass = AFirstPersonBaseHUD::StaticClass();
+}
