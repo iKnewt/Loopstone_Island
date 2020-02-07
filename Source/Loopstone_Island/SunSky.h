e7cfd3813d67de9e016abb35b8620ff2f55eb66c
martinGFA <32432545+martinGFA@users.noreply.github.com>
2020-02-07 13:36:27 +0100

    Added a CPP SunSky. Also added a function to call from C++.

---
+++ Source/Loopstone_Island/SunSky.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SunSky.generated.h"
+
+UCLASS()
+class LOOPSTONE_ISLAND_API ASunSky : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASunSky();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+		void ChangeTimeOfDay();
+
+};
