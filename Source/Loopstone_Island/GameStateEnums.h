b1a6b5d0ae12615d37b4cdb29e89ee989c4f2003
iKnewt <iknewt@gmail.com>
2020-02-07 15:35:35 +0100

    moved enums to separate header

---
+++ Source/Loopstone_Island/GameStateEnums.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Class.h"
+
+
+UENUM(BlueprintType)
+enum class ETimeOfDay : uint8
+{
+	Morning,
+	Afternoon,
+	Evening,
+	Night,
+	None
+};
+
+UENUM(BlueprintType)
+enum class EStory : uint8
+{
+	PartyPlanner,
+	AssistantChef,
+	None
+};
+
+UENUM(BlueprintType)
+enum class EEventType : uint8
+{
+	HasRope,
+	None
+};
+
+UENUM(BlueprintType)
+enum class ETopic : uint8
+{
+	Tape,
+	Rope,
+	Party,
+	None
+};
+
+
