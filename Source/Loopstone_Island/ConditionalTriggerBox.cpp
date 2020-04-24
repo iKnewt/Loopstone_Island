2b77d1548687803caf2c3eb3b554dff40ba8b5b2
iKnewt <iknewt@gmail.com>
2020-04-24 12:06:08 +0200

    fixed some roofs and adjusted some audio volume based on feedback (playtesting needed)

---
+++ Source/Loopstone_Island/ConditionalTriggerBox.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ConditionalTriggerBox.h"
+
+// Sets default values
+AConditionalTriggerBox::AConditionalTriggerBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AConditionalTriggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AConditionalTriggerBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
